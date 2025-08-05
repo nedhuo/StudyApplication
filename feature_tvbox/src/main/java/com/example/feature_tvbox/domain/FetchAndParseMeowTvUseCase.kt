dc7913cea92ae935cffbb4ad3419e8374c3b1b5a
nedhuo <nedhuo@163.com>
2025-08-05 11:52:23 +0800

    清除业务代码，构建纯净的框架，用于后续拓展

--- feature_tvbox/src/main/java/com/example/feature_tvbox/domain/FetchAndParseMeowTvUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.feature_tvbox.domain
-
-import com.example.feature_tvbox.data.TvBoxRepository
-
-class FetchAndParseMeowTvUseCase(private val repository: TvBoxRepository) {
-    suspend operator fun invoke() = repository.fetchAndSaveMeowTvSites()
-} 
