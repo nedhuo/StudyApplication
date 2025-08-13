98dc3bca36679b3c8229b7a57856eae805bed5ab
nedhuo <nedhuo@163.com>
2025-08-13 16:21:18 +0800

    [FEAT]: 移除toast模块

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -136,7 +136,6 @@ object ProjectModules {
     const val libConfig = ":LibConfig"
     const val libDatabase = ":LibDatabase"
     const val libWebView = ":LibWebView"
-    const val libToast = ":LibToast"
     const val featureLogin = ":FeatureLogin"
     const val featureLoginApi = ":FeatureLoginApi"
     const val featureMain = ":FeatureMain"
@@ -156,6 +155,5 @@ object ProjectModules {
         featureLogin,
         featureLoginApi,
         featureMain,
-        ProjectModules.libToast,
     )
 }
